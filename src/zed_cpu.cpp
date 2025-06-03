f9e32d3a3e9f324cfa6310dc83deb889f0fad37f
Shida Xu <xushida08@gmail.com>
2025-06-03 20:26:27 +0100

    720p

--- src/zed_cpu.cpp
+++ src/zed_cpu.cpp
@@ -59,7 +59,8 @@ void ZedCameraNode::CameraInit()
 {
   // Initialize ZED camera
   sl_oc::video::VideoParams params;
-  params.res = sl_oc::video::RESOLUTION::HD1080;
+  // params.res = sl_oc::video::RESOLUTION::HD1080;
+  params.res = sl_oc::video::RESOLUTION::HD720
   params.fps = sl_oc::video::FPS::FPS_30;
   params.verbose = sl_oc::VERBOSITY::INFO;
 
