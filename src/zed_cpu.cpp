51a02c046c3ef828d7bc367c8257fe37a8f2b828
Shida Xu <xushida08@gmail.com>
2025-06-03 19:27:42 +0100

    update 1080P

--- src/zed_cpu.cpp
+++ src/zed_cpu.cpp
@@ -59,7 +59,7 @@ void ZedCameraNode::CameraInit()
 {
   // Initialize ZED camera
   sl_oc::video::VideoParams params;
-  params.res = sl_oc::video::RESOLUTION::VGA;
+  params.res = sl_oc::video::RESOLUTION::HD1080;
   params.fps = sl_oc::video::FPS::FPS_30;
   params.verbose = sl_oc::VERBOSITY::INFO;
 
